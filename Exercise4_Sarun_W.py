7aeba9a5b589ed92ebf753b58d7657ad373ef473
Sarun Wongpisitkhun <ssarun.ww@gmail.com>
2021-01-08 15:17:51 +0700

    Added Exercise 4
    Complete Python 3 Course

---
+++ Exercise4_Sarun_W.py
@@ -0,0 +1,35 @@
+'''
+"หากผู้เรียนกำลังศึกษาอยู่ในชั้นปีที่ 2 มหาวิทยาลัยชื่อดังย่านกลางเมืองที่ตอนนี้กำลังสอบเก็บคะแนนกันอย่างสนุกสนาน 
+แล้ววันนึงได้รับมอบหมายจากอาจารย์ที่ปรึกษาให้พัฒนาโปรแกรมให้กับทางคณะ 
+โดย ระบบดังกล่าวจะมีการเก็บคะแนนรายบุคคลของผู้เรียนแต่ละท่าน ของนักศึกษาชั้นปีที่ 2 เทอม 1" โดยมีรายชื่อวิชาดังนี้
+
+    Foundation English
+    General Business
+    Introduction to Computer Systems
+    Computer Programming
+
+โดยให้ผู้เรียนพัฒนาโปรแกรมโดยสร้างตัวแปรสำหรับเก็บคะแนนผู้เรียนในรายวิชาดังกล่าว 
+โดยคะแนนจะสามารถเป็นตัวเลขทศนิยมได้ และ เมื่อได้ตัวแปรมาแล้วให้ทำการแสดงผลออกมาให้ผู้ใช้งานได้เห็นในรูปแบบ
+
+--- Your Score ---
+
+Foundation English : คะแนนที่ได้
+
+General Business : คะแนนที่ได้
+
+Introduction to Computer Systems : คะแนนที่ได้
+
+Computer Programming : คะแนนที่ได้
+
+'''
+
+fe_point = 0.0
+gb_point = 0.0
+ics_point = 0.0
+cp_point = 0.0
+
+print("--- Your Score ---")
+print("Foundation English : ", fe_point)
+print("General Business : ", gb_point)
+print("Introduction to Computer Systems : ", ics_point)
+print("Computer Programming : ", cp_point)
