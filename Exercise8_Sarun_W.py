f9f1199cdcfdb64dd53b186418ca79d72507cb2b
Sarun Wongpisitkhun <ssarun.ww@gmail.com>
2021-02-02 12:50:27 +0700

    Added Exercise8

---
+++ Exercise8_Sarun_W.py
@@ -0,0 +1,50 @@
+"""
+
+เขียนโปรแกรมร้านขายของโดยดัดแปลงจากโปรแกรมใน Lecture 40
+ในการเข้าใช้งานโปรแกรมให้ผู้ล็อคอินโดยใช้ Username และ Password(ผู้เรียนกำหนดเอง)
+หากสำเร็จ โปรแกรมจะขึ้นหน้าต้อนรับและแสดงรายการสินค้าพร้อมราคา (ผู้เรียนกำหนดเอง)
+เมื่อเลือกสินค้าที่ต้องการเรียบร้อยแล้ว โปรแกรมจะถามจำนวนที่ต้องการซื้อ
+หลังจากผู้ซื้อเลือกเรียบร้อยแล้ว โปรแกรมจะทำการแสดงสรุปราคารวมของรายการสั่งซื้อทั้งหมด
+
+"""
+
+usernameInput = input("Username: ")
+passwordInput = input("Password: ")
+
+totalPrices = 0
+
+
+if usernameInput == "admin" or usernameInput == "client":
+    if passwordInput == "1234":
+        print("# Login Successes")
+        # Product and Price.
+        applePrice = 3
+        bananaPrice = 6
+        coconutPrice = 20
+        doraemonPrice = 360
+        
+        print("------ Welcome to Market ------")
+        print("1.Apple                    " + str(applePrice) + "฿")
+        print("2.Banana                   " + str(bananaPrice) + "฿")
+        print("3.Coconut                 " + str(coconutPrice) + "฿")
+        print("4.Doraemon               " + str(doraemonPrice) + "฿")
+        print("Please select your products per each.")
+
+        if input("Apple? (Y/N) :") == "Y":
+            totalPrices += int(input("How many do you want?: ")) * applePrice
+        if input("Banana? (Y/N) :") == "Y":
+            totalPrices += int(input("How many do you want?: ")) * bananaPrice
+        if input("Coconut? (Y/N) :") == "Y":
+            totalPrices += int(input("How many do you want?: ")) * coconutPrice
+        if input("Doraemon? (Y/N) :") == "Y":
+            totalPrices += int(input("How many do you want?: ")) * doraemonPrice
+
+        if totalPrices != 0:
+            print("Total product price: " + str(totalPrices) + "฿\nTHANK YOU, Have a nice day sir. :D")
+        else:
+            print("See you next time. :D")
+
+    else:
+        print("# Username or password invalid.")
+else:
+    print("# Username or password invalid.")
